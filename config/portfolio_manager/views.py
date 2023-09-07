8798f61d9dd4668b28b60fc2a6ade3d6b7e80566
Dawlau <blahoviciandrei1@gmail.com>
2023-09-07 23:57:42 +0200

    Generate the django app and add some dummy routes

---
+++ config/portfolio_manager/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Portfolio
+
+def home(request):
+    return render(request, "home.html")
+
+def portfolio(request):
+    portfolio = Portfolio.objects.first()
+    return render(request, "portfolio.html", {"portfolio": portfolio})
