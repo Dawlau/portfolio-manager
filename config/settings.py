9437c493f50503e518f12028a77899ebc37e440d
Dawlau <blahoviciandrei1@gmail.com>
2023-09-11 11:18:49 +0200

    Removed authentication app

--- config/settings.py
+++ config/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "portfolio_manager",
-    "authentication"
+    "portfolio_manager"
 ]
 
 MIDDLEWARE = [
